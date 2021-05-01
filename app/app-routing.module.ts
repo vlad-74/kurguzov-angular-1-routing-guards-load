2461c65f9b452b65c6e54d6d3a000f3b83f27f2a
vlad-74 <kurguzov74@gmail.com>
2021-05-02 02:26:56 +0300

    1 file modified

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,41 +1,44 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 
-import { MainComponent } from './components/main.component';
-import { HelloComponent } from './components/hello/hello.component';
+import { MainComponent } from "./components/main.component";
+import { HelloComponent } from "./components/hello/hello.component";
 
-import { HelloResolver } from './guards/hello-resolver';
-import { HelloGuard } from './guards/activate.guard';
-import { ExitHelloGuard } from './guards/exit-hello.guard';
+import { HelloResolver } from "./guards/hello-resolver";
+import { HelloGuard } from "./guards/activate.guard";
+import { ExitHelloGuard } from "./guards/exit-hello.guard";
 
 import { CustomPreloadService } from "./modules/preload/custom-preload.service";
 
-
 const routes: Routes = [
   { path: "main", pathMatch: "full", component: MainComponent },
-  { path: 'hello', 
-    component: HelloComponent, 
-    canActivate: [HelloGuard], 
-    canDeactivate: [ExitHelloGuard], 
-    resolve: { caunter: HelloResolver}
+  {
+    path: "hello",
+    component: HelloComponent,
+    canActivate: [HelloGuard],
+    canDeactivate: [ExitHelloGuard],
+    resolve: { caunter: HelloResolver }
   },
   {
-    path: 'children',
-    loadChildren: () => import('./modules/children/children.module').then(m => m.ChildrenModule)
+    path: "children",
+    loadChildren: () =>
+      import("./modules/children/children.module").then(m => m.ChildrenModule)
   },
-  { 
-    path: 'home',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./modules/home/home.module").then(m => m.HomeModule)
   },
-  { 
-    path: 'preload', 
-    loadChildren: () => import('./modules/preload/preload.module').then(m => m.PreloadModule),
-    data: { preload: true } 
+  {
+    path: "preload",
+    loadChildren: () =>
+      import("./modules/preload/preload.module").then(m => m.PreloadModule),
+    data: { preload: true }
   },
-   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/children'
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/children"
   }
 ];
 
@@ -48,6 +51,6 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
   declarations: [MainComponent, HelloComponent],
-  providers: [HelloResolver, HelloGuard, ExitHelloGuard],
+  providers: [HelloResolver, HelloGuard, ExitHelloGuard]
 })
-// export class AppRoutingModule {}
+export class AppRoutingModule {}
